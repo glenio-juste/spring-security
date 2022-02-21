c8b8255fab520f5e6469dbb60fa828b670d639e2
glenio-juste <glenio.juste@gmail.com>
2022-02-21 20:53:26 -0300

    Project commit

---
+++ juste-spring-security/src/main/java/juste/spring/security/JusteSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package juste.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JusteSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JusteSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ juste-spring-security/src/test/java/juste/spring/security/JusteSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package juste.spring.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JusteSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
